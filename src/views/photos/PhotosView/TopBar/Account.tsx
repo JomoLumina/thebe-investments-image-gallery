9d5ddddf985397bcd72b3eb1989c52d79df32fff
Jomo Lumina <luminajj@yahoo.com>
2022-02-28 04:55:56 +0200

    added files back

---
+++ src/views/photos/PhotosView/TopBar/Account.tsx
@@ -0,0 +1,105 @@
+import React, {
+  useRef,
+  useState
+} from 'react';
+import type { FC } from 'react';
+import { useHistory } from 'react-router-dom';
+import {
+  Avatar,
+  Box,
+  ButtonBase,
+  Hidden,
+  Menu,
+  MenuItem,
+  Typography,
+  makeStyles
+} from '@material-ui/core';
+import useAuth from 'src/hooks/useAuth';
+
+const useStyles = makeStyles((theme) => ({
+  avatar: {
+    height: 32,
+    width: 32,
+    marginRight: theme.spacing(1)
+  },
+  popover: {
+    width: 200,
+    zIndex: 20
+  },
+  username:{
+    fontSize: 14,
+    letterSpacing: -0.05,
+  }
+}));
+
+const Account: FC = () => {
+  const classes = useStyles();
+  const history = useHistory();
+  const ref = useRef<any>(null);
+  const { user, logout } = useAuth();
+  const [isOpen, setOpen] = useState<boolean>(false);
+
+  const handleOpen = (): void => {
+    setOpen(true);
+  };
+
+  const handleClose = (): void => {
+    setOpen(false);
+  };
+
+  const handleLogout = async (): Promise<void> => {
+    try {
+      handleClose();
+      await logout();
+      history.push('/');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <>
+      <Box
+        display="flex"
+        alignItems="center"
+        component={ButtonBase}
+        onClick={handleOpen}
+        // @ts-ignore
+        ref={ref}
+      >
+        <Avatar
+          alt="User"
+          className={classes.avatar}
+          src={user.avatar}
+        />
+        <Hidden xsDown>
+          <Typography
+            variant="h6"
+            color="inherit"
+            className={classes.username}
+          >
+            {user.name}
+          </Typography>
+        </Hidden>
+      </Box>
+      <Menu
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center'
+        }}
+        keepMounted
+        PaperProps={{ className: classes.popover }}
+        getContentAnchorEl={null}
+        anchorEl={ref.current}
+        open={isOpen}
+      >
+        <MenuItem onClick={handleLogout}>
+          Logout
+        </MenuItem>
+      </Menu>
+    </>
+  );
+}
+
+export default Account;
